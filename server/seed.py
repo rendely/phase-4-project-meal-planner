2789071f56ac9b1732dea0ec4b08fff30c1ab784
matthew rendely <matthew.rendely@gmail.com>
2023-08-11 20:59:15 -0700

    update seed.py with more ingredients

--- server/seed.py
+++ server/seed.py
@@ -18,7 +18,14 @@ if __name__ == '__main__':
         user = User(username='admin')
         user.password_hash = 'admin'
 
-        for i in ['pasta noodle', 'pasta sauce', 'rice', 'chicken']:
+        for i in ['pasta noodle', 'pasta sauce', 'rice', 'chicken', 'ground beef', 'bread', \
+                'milk', 'eggs', 'butter', 'flour', 'sugar', 'salt', 'black pepper', 'olive oil', \
+                'tomato', 'cucumber', 'lettuce', 'carrots', 'broccoli', 'bell peppers', 'onions',\
+                'garlic', 'potatoes', 'canned beans', 'canned tomatoes', 'canned tuna', 'cereal',\
+                'oats', 'yogurt', 'cheese', 'frozen vegetables', 'frozen fruits', 'peanut butter',\
+                'jam', 'honey', 'breadcrumbs', 'mayonnaise', 'mustard', 'ketchup', 'soy sauce', \
+                'vinegar', 'canned soup', 'salad dressing', 'pasta salad ingredients', \
+                'tortilla wraps', 'salsa', 'granola bars', 'nuts', 'dried fruits', 'chocolate chips']:
             ingredient = Ingredient(name=i)
             ingredient.user = user
             db.session.add(ingredient)
