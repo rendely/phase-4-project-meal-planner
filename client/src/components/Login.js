2d1c137445a5474fa7204ae9ac7844e11946bbbc
matthew rendely <matthew.rendely@gmail.com>
2023-08-11 17:00:33 -0700

    Add formik validation to login flow

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,41 @@
 import React, {useState} from 'react'
-import { Container, Segment, Form, Checkbox, Button } from 'semantic-ui-react'
+import { Container, Segment, Form, Button } from 'semantic-ui-react'
+import {useFormik} from "formik";
+import * as yup from "yup";
+
 function Login({ setUser }) {
 
-  const [formData, setFormData] = useState({username: 'admin', password: 'admin'})
+  const [formData, setFormData] = useState({username: 'admin', password: 'admin'});
+
+  const formSchema = yup.object().shape({
+    username: yup.string().required("Must enter a username"),
+    password: yup.string().required("Must enter a password").min(3, "At least 3 characters"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      }).then(r => {
+        if (r.status === 201) return r.json()
+        throw new Error('Login failed')
+      })
+      .then(d => {
+        if (d.id) setUser(d.id)
+      })
+      .catch((error) => console.log(error));
+    },
+  });
+
 
   function handleSignup(e) {
     e.preventDefault();
@@ -52,14 +85,14 @@ function Login({ setUser }) {
         <Form>
           <Form.Field>
             <label>Username</label>
-            <input name='username' placeholder='Username' onChange={handleChange} value={formData.username} />
+            <input name='username' placeholder='Username' onChange={formik.handleChange} value={formik.values.username} />
           </Form.Field>
           <Form.Field>
             <label>Password</label>
-            <input name='password' placeholder='Password' type='password' onChange={handleChange}  value={formData.password}/>
+            <input name='password' placeholder='Password' type='password' onChange={formik.handleChange}  value={formik.values.password}/>
           </Form.Field>
           <Button type='button' onClick={handleSignup}>Signup</Button>
-          <Button type='button' onClick={handleLogin}>Login</Button>
+          <Button type='button' onClick={formik.handleSubmit}>Login</Button>
         </Form>
       </Segment>
     </Container>
