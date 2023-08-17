ed4f87fec76ff23f887c020d083e128a2697e8ca
matthew rendely <matthew.rendely@gmail.com>
2023-08-16 19:28:54 -0700

    Add icon to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Menu } from 'semantic-ui-react'
+import { Menu, Icon } from 'semantic-ui-react'
 import { NavLink, Link } from 'react-router-dom'
 
 function NavBar({ onLogout }) {
@@ -6,7 +6,10 @@ function NavBar({ onLogout }) {
   return (<>
     <div style={{ marginBottom: '70px' }}>
       <Menu widths={8} fixed='top'>
-        <Menu.Item as='span' style={{fontWeight: '800'}} >Meal Planner</Menu.Item>
+        <Menu.Item as='span' style={{fontWeight: '800'}} >
+          <Icon name='clipboard outline' />
+          Meal Planner
+          </Menu.Item>
         <Menu.Item as={NavLink} to="/" exact name="Home"></Menu.Item>
         <Menu.Item as={NavLink} to="/meals" name="Meals"></Menu.Item>
         <Menu.Item as={NavLink} to="/ingredients" name="Ingredients"></Menu.Item>
