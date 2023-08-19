1b9e77688507bab188ab767513996eef42149b1e
matthew rendely <matthew.rendely@gmail.com>
2023-08-18 23:56:32 -0700

    Consolidate the app name and home button on nav to be a single button

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,11 +6,10 @@ function NavBar({ onLogout }) {
   return (<>
     <div style={{ marginBottom: '70px' }}>
       <Menu widths={8} fixed='top'>
-        <Menu.Item as='span' style={{fontWeight: '800'}} >
+        <Menu.Item as={NavLink} to="/" exact>
           <Icon name='clipboard outline' />
           Meal Planner
-          </Menu.Item>
-        <Menu.Item as={NavLink} to="/" exact name="Home"></Menu.Item>
+        </Menu.Item>
         <Menu.Item as={NavLink} to="/meals" name="Meals"></Menu.Item>
         <Menu.Item as={NavLink} to="/ingredients" name="Ingredients"></Menu.Item>
         <Menu.Item as={Link} to="/" name="Logout" onClick={onLogout}></Menu.Item>
