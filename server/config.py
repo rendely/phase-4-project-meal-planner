2e3298f1dec2ddfa7e1d203ec98db880a4db4c7c
matthew rendely <matthew.rendely@gmail.com>
2023-08-20 09:57:40 -0700

    Change database to postgresql on render"

--- server/config.py
+++ server/config.py
@@ -1,4 +1,5 @@
 # Standard library imports
+import os
 
 # Remote library imports
 from flask_bcrypt import Bcrypt
@@ -14,7 +15,8 @@ from sqlalchemy import MetaData
 # Instantiate app, set attributes
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
