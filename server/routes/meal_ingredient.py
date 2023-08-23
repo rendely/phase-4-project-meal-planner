a3fb7839db17248c8e949660d9c1572784d9d475
matthew rendely <matthew.rendely@gmail.com>
2023-08-22 20:19:13 -0700

    Add get to meal_ingredient

--- server/routes/meal_ingredient.py
+++ server/routes/meal_ingredient.py
@@ -4,15 +4,10 @@ from config import app, db, api
 from models.models import meal_ingredient, Ingredient, Meal
 
 class MealAndIngredient(Resource):
-    def delete(self, meal_id, ingredient_id):
-        try: 
-            db.session.query(meal_ingredient).filter_by(meal_id = meal_id, ingredient_id =ingredient_id).delete()
-            db.session.commit()
-            return {}, 200
-        except IntegrityError:
-            print(e)
-            return {'error': '422'}, 422
-    
+    def get(self, meal_id, ingredient_id):
+        meal_ingredient = db.session.query(meal_ingredient).filter_by(meal_id = meal_id, ingredient_id =ingredient_id).first()
+        return make_response(meal_ingredient.to_dict(), 200)
+
     def post(self, meal_id, ingredient_id):
         ingredient_id = request.get_json().get('ingredient_id')
         try:
@@ -24,5 +19,13 @@ class MealAndIngredient(Resource):
         except IntegrityError:
             print(e)
             return {'error': '422'}, 422
-
+    def delete(self, meal_id, ingredient_id):
+        try: 
+            db.session.query(meal_ingredient).filter_by(meal_id = meal_id, ingredient_id =ingredient_id).delete()
+            db.session.commit()
+            return {}, 200
+        except IntegrityError:
+            print(e)
+            return {'error': '422'}, 422
+    
 api.add_resource(MealAndIngredient, '/api/meals/<int:meal_id>/ingredients/<int:ingredient_id>', endpoint='/api/meals/id/ingredient/id')
