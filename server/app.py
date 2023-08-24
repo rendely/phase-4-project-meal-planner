4316365e8dd94f27f6f9badb3e2360e07e9639cb
matthew rendely <matthew.rendely@gmail.com>
2023-08-23 19:40:02 -0700

    Fix so doesn't 404 on pages that aren't index

--- server/app.py
+++ server/app.py
@@ -11,13 +11,9 @@ from server.config import app
 from server.models.models import *
 from server.routes.routes import *
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
 @app.errorhandler(404)
 def not_found(e):
-    return '404 Not found'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
